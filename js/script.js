34fe919334190719f83c17542be3d9cc75e8f8e6
unknown <abrordevaxmedov@gmail.com>
2024-05-05 18:18:55 +0300

    accorsian complete

--- js/script.js
+++ js/script.js
@@ -8,3 +8,25 @@ const menuBtn = (evt) => {
   elWrapper.classList.toggle("bg-color");
 };
 elMenuBtn.addEventListener("click", menuBtn);
+
+// Faqs accordian
+
+document.addEventListener("click", (e) => {
+  if (
+    !e.target.matches(".howitwporks__header") &&
+    !e.target.matches(".faqs-btn")
+  )
+    return;
+  let card = e.target.closest(".howitworks__card");
+  if (!card) return;
+  let body = card.querySelector(".howitworks__body");
+  let button = card.querySelector(".faqs-btn");
+  card.classList.toggle("active");
+  if (card.classList.contains("active")) {
+    body.style.display = "block";
+    button.style.backgroundImage = `url("../assets/image/about-arrow-down.png")`;
+  } else {
+    body.style.display = "none";
+    button.style.backgroundImage = `url("../assets/image/about-faqs-icon-top.png")`;
+  }
+});
